f29755e44efffb8525f2d14398b3a0c20afce43c
asgerbirk <asger.birk@gmail.com>
2023-02-16 12:26:45 +0100

    done

--- first_server/app.js
+++ first_server/app.js
@@ -1,7 +1,29 @@
 const express = require("express")
 const app = express();
 
+app.use(express.json());
 
+const birdArray= [{
+    id: 1,
+    name:"john",
+    size:"big",
+    color:"yellow"
+},
+    {
+        id: 2,
+        name:"zanduu",
+        size: "small",
+        color: "blue"
+    }
+]
+app.get("/api/v1/getAllBirds", (req,res) =>{
+    res.send({birdArray})
+})
+
+app.get("/api/v1/getBirdById/:id", (req,res) =>{
+    const bird = birdArray.find(i => i.id === parseInt(req.params.id))
+    res.send(bird)
+})
 
 
 app.listen(8080)
